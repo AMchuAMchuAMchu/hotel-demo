dcedb64f5d34a384f682edd7e4e32a012c96cd17
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-13 21:47:09 +0800

    shino_hecate

--- src/main/java/cn/itcast/hotel/web/HotelController.java
+++ src/main/java/cn/itcast/hotel/web/HotelController.java
@@ -30,7 +30,7 @@ public class HotelController {
         return iHotelService.search(requestParams);
     }
 
-    @PostMapping("/filter")
+    @PostMapping("/filters")
     public PageResult filter(@RequestBody RequestParams requestParams){
         return iHotelService.search(requestParams);
     }
